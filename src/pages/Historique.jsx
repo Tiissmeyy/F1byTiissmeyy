9a743638560a628a58831a2f99b1f72f6812f629
Tiissmeyy <124519303+Tiissmeyy@users.noreply.github.com>
2023-04-22 19:47:38 +0200

    delete scroll console.log

--- src/pages/Historique.jsx
+++ src/pages/Historique.jsx
@@ -4,25 +4,11 @@ import date from "../data/date.json"
 import style from "./Historique.module.css"
 
 const Historique = () => {
-    const [currentPosition, setCurrentPosition] = useState(window.scrollY);
-    const dateRefs = useRef([]);
-
-    useEffect(() => {
-        function handleScroll() {
-          setCurrentPosition(window.scrollY);
-          console.log('Défilement détecté !');
-        }
-    
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-        };
-      }, []);
 
     
     const dateElements = date.dates.map((el, index)=> {
         return(
-            <DateElement ref={ref => dateRefs.current[index] = ref} key={index} date={el.date} event={el.evenement} currentPosition={currentPosition} />
+            <DateElement key={index} date={el.date} event={el.evenement} />
         )
     })
 
